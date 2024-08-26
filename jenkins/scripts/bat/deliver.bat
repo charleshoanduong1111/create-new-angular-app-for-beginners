a11a0c3cd9592c834ab29e25a52593eabdd3bf1a
charleshoanduong1111 <153458964+charleshoanduong1111@users.noreply.github.com>
2024-08-26 11:18:18 +0200

    Update deliver.bat

--- jenkins/scripts/bat/deliver.bat
+++ jenkins/scripts/bat/deliver.bat
@@ -1,7 +1,8 @@
 @echo off
 call npm run build
 call npm run erve
-echo Done > .done.txt
-type .done.txt
+REM echo Done > .done.txt
+REM type .done.txt
 echo 'Now...'
 echo 'Visit http://localhost:4200 to see your Node.js/Angular application in action.'
+curl http://localhost:4200
