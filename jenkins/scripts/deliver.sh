952bea65bc5c37bcb82c6dd0d20ba3709622b520
charleshoanduong1111 <153458964+charleshoanduong1111@users.noreply.github.com>
2024-08-26 10:54:09 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,8 +1,8 @@
-@echo off 
+@echo on
 call npm run build
 call npm run serve
-echo $! > .pidfile
-type .pidfile
+echo Done > done.txt
+type done.txt
 pause
 echo 'Now...'
 echo 'Visit http://localhost:4200 to see your Node.js/Angular application in action.'
